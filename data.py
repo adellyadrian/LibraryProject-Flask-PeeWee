4c57e8265151bfa78c04a2ee01c5604b49542f66
Adelina <126407871+adellyadrian@users.noreply.github.com>
2023-09-18 09:54:30 +0400

    Update data.py

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-from peewee import SqliteDatabase, Model, PrimaryKeyField, CharField, IntegerField, AutoField
+from peewee import SqliteDatabase, Model, PrimaryKeyField, CharField, IntegerField, ForeignKeyField, DateTimeField, BooleanField
 
 
 db = SqliteDatabase('datab.db')
@@ -10,13 +10,15 @@ class BaseModel(Model):
 
 
 class Books(BaseModel):
-    image = CharField(null=False)
+    image = CharField()
     id = PrimaryKeyField(null=False)
     title = CharField(max_length=100, null=False)
     year = IntegerField(null=False)
     author = CharField(max_length=100, null=False)
     content = CharField(max_length=100, null=False)
     isbn = IntegerField(null=False)
+    status = CharField()
+    available = BooleanField(default=True) 
 
     class Meta:
         table_name = 'books'
@@ -33,6 +35,16 @@ class Student(BaseModel):
         table_name = 'students'
 
 
+class BorrowedBooks(BaseModel):
+    student = ForeignKeyField(Student, backref='borrowed_books')
+    book = ForeignKeyField(Books, backref='borrowed_by_students')
+    borrow_date = DateTimeField()
+
+    class Meta:
+        table_name = 'borrowed_books'
+        
+
 def create_tables():
     with db:
-        db.create_tables([Books, Student])
+        db.create_tables([Books, Student, BorrowedBooks])
+        
